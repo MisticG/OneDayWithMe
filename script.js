06c1b2a64b6037b2c787b6e0132230c7d1ce1f98
Your Name <you@example.com>
2018-10-05 15:24:33 +0200

    Finished!

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-function changeButton() {
 
-    var chgButton = document.getElementById("myButton");
     
-    if(chgButton.value == "Tuy-Vi kl 8." ) chgButton.value = "Tuy-Vi kl 12."; 
-    else if(chgButton.value == "Tuy-Vi kl 12.") chgButton.value = "Tuy-Vi kl 16.";
-    else if(chgButton.value == "Tuy-Vi kl 16.") chgButton.value = "Tuy-Vi kl 8.";
-    else chgButton.value;
+
+var chgButton = document.getElementById("myButton");    
+
+function changeButton() {
+    
+    var chgButton = document.getElementById("myButton");
+
+    var image = document.getElementById("image");
+    
+    console.log(image)
+    
+    if(chgButton.value == "Tuy-Vi kl 8." ) {
+        chgButton.value = "Tuy-Vi kl 12.";
+        image.src = "sitWithLaptop.gif"
+    }
+    else if(chgButton.value == "Tuy-Vi kl 12.") {
+        chgButton.value = "Tuy-Vi kl 16.";
+        image.src = "zombie.gif" 
+    }
+    else if(chgButton.value == "Tuy-Vi kl 16.") { 
+        chgButton.value = "Tuy-Vi kl 8.";
+        image.src = "OnWayToSchool.gif"
+    }
+
+
+    console.log(chgButton.value);
+
 };
+
+
+ 
